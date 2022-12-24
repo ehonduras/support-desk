750a47744ba78ac8a346f9c685d129146c9584ed
ldrljaca <ermin.h@protonmail.com>
2022-12-24 20:55:29 +0100

    added prettier and frontend started

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,17 @@
-const express = require('express');
-const colors = require('colors');
-const dotenv = require('dotenv').config();
-const {errorHandler} = require('./middleware/errorMiddleware');
-const connectDB = require('./config/db');
+const express = require("express");
+const colors = require("colors");
+const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
 const PORT = process.env.PORT || 5000;
 
 connectDB();
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
-app.use('/api/users', require('./routes/userRoutes')); 
+app.use("/api/users", require("./routes/userRoutes"));
 
 app.use(errorHandler);
 
