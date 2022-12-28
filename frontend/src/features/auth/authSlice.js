930932adc7f05014786747db03a03314f971a9d4
ldrljaca <ermin.h@protonmail.com>
2022-12-28 14:20:42 +0100

    axios issues

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,4 +1,5 @@
 import {createSlice, createAsyncThunk} from '@reduxjs/toolkit';
+import authService from './authService';
 
 const initialState = {
     user: null,
@@ -9,7 +10,13 @@ const initialState = {
 }
 
 export const register = createAsyncThunk('auth/register', async (user, thunkAPI) => {
-    console.log(user);
+    try {
+        return await authService.register(user);
+    } catch (error) {
+        const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString();
+
+        return thunkAPI.rejectWithValue(message);
+    }
 })
 
 export const login = createAsyncThunk('auth/login', async (user, thunkAPI) => {
