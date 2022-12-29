734d4d59feb0b90ca1b572e2bbab55ea780ad133
ldrljaca <ermin.h@protonmail.com>
2022-12-29 11:11:19 +0100

    full crud functionalities

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/users", require("./routes/userRoutes"));
+app.use("/api/tickets", require("./routes/ticketRoutes"));
 
 app.use(errorHandler);
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`)); 
