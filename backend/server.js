b0aaea3e4eab6f90309bc34f0b14448a1a639a73
ldrljaca <ermin.h@protonmail.com>
2023-01-19 10:50:20 +0100

    deployment ready

--- backend/server.js
+++ backend/server.js
@@ -16,19 +16,19 @@ app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/tickets", require("./routes/ticketRoutes"));
 
 //serve frontend
-if (process.env.NODE_ENV === 'production') {
-    // Set build folder as static
-    app.use(express.static(path.join(__dirname, '../frontend/build')))
-  
-    // FIX: below code fixes app crashing on refresh in deployment
-    app.get('*', (_, res) => {
-      res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
-    })
-  } else {
-    app.get('/', (_, res) => {
-      res.status(200).json({ message: 'Welcome to the Support Desk API' })
-    })
-  }
+// if (process.env.NODE_ENV === "production") {
+//   // Set build folder as static
+//   app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+//   // FIX: below code fixes app crashing on refresh in deployment
+//   app.get("*", (_, res) => {
+//     res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+//   });
+// } else {
+//   app.get("/", (_, res) => {
+//     res.status(200).json({ message: "Welcome to the Support Desk API" });
+//   });
+// }
 
 app.use(errorHandler);
 
