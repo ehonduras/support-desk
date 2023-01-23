de0787e3090de47690afb797c18416449445d5ea
ldrljaca <ermin.h@protonmail.com>
2023-01-23 13:22:38 +0100

    new try

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -11,9 +11,12 @@ const Home = () => {
       </section>
       <Link to="/newticket" className="btn btn-reverse btn-block">
         <FaQuestionCircle /> Create New Ticket
-        {/* {
-          window.pe.track('homePageVisit', { propertyName: 'homePageVisit' })
-        } */}
+        {
+          console.log(window.pe, window.liveChat)
+        }
+        {
+          console.log(window.document.pe, window.document.liveChat)
+        }
       </Link>
       <Link to="/tickets" className="btn btn-block">
         <FaTicketAlt /> View My Tickets  
