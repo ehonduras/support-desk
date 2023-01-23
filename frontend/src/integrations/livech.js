47c95eb92048e236ab6ce4633feaf72d8a805ed7
ldrljaca <ermin.h@protonmail.com>
2023-01-23 10:23:23 +0100

    not sure

--- frontend/src/integrations/livech.js
+++
@@ -1,7 +0,0 @@
-(function(I,n,f,o,b,i,p){
-    I[b]=I[b]||function(){(I[b].q=I[b].q||[]).push(arguments)};
-    I[b].t=1*new Date();i=n.createElement(f);i.async=1;i.src=o;
-    p=n.getElementsByTagName(f)[0];p.parentNode.insertBefore(i,p)})
-    (window,document,'script','https://livechat.infobip.com/widget.js','liveChat');
-    
-    liveChat('init', process.env.LC_SECRET);
